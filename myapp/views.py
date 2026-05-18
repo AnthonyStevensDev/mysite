515af28967a9baca642430fc80c5b7d5f68f7f58
Anthony_Stevens <antstevensdev@gmail.com>
2026-05-18 21:02:37 +0100

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'myapp/holding.html') # <--- holding page
+    # return render(request, 'myapp/main.html') # <--- main page
