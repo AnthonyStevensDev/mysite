9a96c8b2fd63083b7c9e5bf6a280185db3831bd5
Anthony_Stevens <antstevensdev@gmail.com>
2026-05-18 23:11:21 +0100

    Fixed issue with requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
