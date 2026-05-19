7c0b83016463cb994f9309406cbb39e74f064468
Anthony_Stevens <antstevensdev@gmail.com>
2026-05-19 17:19:58 +0100

    Fix static files settings for production

--- mysite/settings_prod.py
+++ mysite/settings_prod.py
@@ -3,4 +3,6 @@ from .settings import *
 DEBUG = False
 ALLOWED_HOSTS = ['anthonystevens.dev', 'www.anthonystevens.dev', '.vercel.app']
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
