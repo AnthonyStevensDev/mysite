ca2000bf78a28fcdc70a7c8e51b80157e132acf7
Anthony_Stevens <antstevensdev@gmail.com>
2026-06-30 15:06:31 +0100

    Turn off holding page

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'myapp/holding.html') # <--- holding page
-    # return render(request, 'myapp/index.html') # <--- main page
+    # return render(request, 'myapp/holding.html') # <--- holding page
+    return render(request, 'myapp/index.html') # <--- main page
